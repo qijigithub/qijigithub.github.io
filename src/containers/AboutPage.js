7d4771ddbf3f5e38314ec07e33148ef50e5117f9
qijigithub <qijilearn@gmail.com>
2021-02-27 17:41:15 -0600

    add still building

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const AboutPage = () => {
     return (
         <div>
-            This is about page
+            This is about page, still building...
         </div>
     )
 }
--- src/containers/ProjectsPage.js
+++ src/containers/ProjectsPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ProjectsPage = () => {
     return (
         <div>
-           this is projects page 
+           this is projects page, still building...
         </div>
     )
 }
