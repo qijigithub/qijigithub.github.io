a71b3faa1501d2ef0063b6a589218cfaac4fd2f8
qijigithub <qijilearn@gmail.com>
2021-03-08 10:10:34 -0600

    project

--- src/components/data/AboutData.js
+++ src/components/data/AboutData.js
@@ -12,8 +12,9 @@ export const AboutData = {
     alt: 'profile',
     heading:'About Me',
     paragraph1:'Hi, I\'m Ji. I am a Development Anaylst, focusing on ReactJS and flutter development. I also interested in Data Science. I graduated from DePaul university with a master degree, majoring in computer science. During my study, I am focus on gaining hands - on experience in the area of data science, machine learning algorithm study.',
-    paragraph2: 'I am a Development Anaylst and Data Scientist. If you have project or if you want\
-     make a software for your business contact me. I make as soon as possible.',
+    paragraph2: 'I am a Development Anaylst and Data Scientist.',
+    //  If you have project or if you want\
+    //  make a software for your business contact me. I make as soon as possible.',
     buttonLabel:'View Resume',
     reverse:false,
     delay:100,
