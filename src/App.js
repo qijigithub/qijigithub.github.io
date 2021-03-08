70b5a21f1289d255eaa8e63a002400bba4f70005
qijigithub <qijilearn@gmail.com>
2021-03-07 23:52:09 -0600

    g

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Layout } from './components/layout/Layout';
+import ReactGa from 'react-ga';
+import React, {useEffect} from 'react';
 
 function App() {
+useEffect(() => {
+  ReactGa.initialize('G-SN55C3MXN3')
+  ReactGa.pageview('/')
+}, [])
+
   return (
     <div>
       <Layout/>
